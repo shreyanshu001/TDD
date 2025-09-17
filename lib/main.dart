5a44e23ff3c7fd45b2aab918b11efc7797481a8c
shreyanshu001 <shuklashreyanshu04@gmail.com>
2025-09-17 21:56:36 +0530

    step 1

---
+++ lib/main.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget { const MyApp({super.key}); @override Widget build(BuildContext context) { return MaterialApp( title: 'String Calculator TDD Kata', theme: ThemeData( primarySwatch: Colors.blue, ), home:, ); } }
